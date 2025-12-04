f83604a4c268c5337fbeffef7f60de831dec3acd
LeeJJang1 <wjdgusdl0507@skuniv.ac.kr>
2025-12-04 14:04:49 +0900

    Change in master branch

--- GitHW/src/mygit/YourClass.java
+++ GitHW/src/mygit/YourClass.java
@@ -1,8 +1,9 @@
 package mygit;
 
 public class YourClass {
-	public void sayHelloo(String toWhom) {
+	public void sayHello(String toWhom) {
 		System.out.println("Hello" + toWhom);
+		System.out.println("Your changed in master branch");
 	}
 
 }
