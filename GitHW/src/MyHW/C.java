1a75cbb5b0b7d03000e8c9a06afef9b9444d1d66
LeeJJang1 <wjdgusdl0507@skuniv.ac.kr>
2025-12-14 18:29:31 +0900

    수정된 A,C

--- GitHW/src/MyHW/C.java
+++ GitHW/src/MyHW/C.java
@@ -4,6 +4,8 @@ package MyHW;
 public class C {
  public static void main(String[] args) {
      System.out.println("학번은 2021301065");
+     System.out.println("학번은 2021301065, 이름은 이정현");
+     
  }
 }
 
