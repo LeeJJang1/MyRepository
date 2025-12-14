000e9999140a5e6a47ad96f84a28ac46dd28d153
LeeJJang1 <wjdgusdl0507@skuniv.ac.kr>
2025-12-14 18:30:51 +0900

    master에서 C 수정

--- GitHW/src/MyHW/C.java
+++ GitHW/src/MyHW/C.java
@@ -4,6 +4,7 @@ package MyHW;
 public class C {
  public static void main(String[] args) {
      System.out.println("학번은 2021301065");
+     System.out.println("학번은 2021301065,여기는 master branch");
  }
 }
 
