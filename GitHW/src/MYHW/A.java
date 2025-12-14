7838d95be9c43238d8bf94c810665b660000ab8b
LeeJJang1 <wjdgusdl0507@skuniv.ac.kr>
2025-12-14 18:23:48 +0900

    수정한 A,C

--- GitHW/src/MYHW/A.java
+++ GitHW/src/MYHW/A.java
@@ -3,5 +3,6 @@ package MYHW;
 public class A {
  public void say() {
      System.out.println("내이름은 이정현입니다.");
+     System.out.println("내이름은 이정현이고 학번은 2021301065입니다.");
  }
 }
--- GitHW/src/MYHW/C.java
+++ GitHW/src/MYHW/C.java
@@ -3,5 +3,7 @@ package MYHW;
 public class C {
  public static void main() {
      System.out.println("학과는 소프트웨어학과입니다.");
+     System.out.println("학과는 소프트웨어학과이고 이름은 이정현입니다.");
+     
  }
 }
